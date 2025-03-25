3cf13713f2d16acac4e6565be838d3e22cbd6655
Aruna-Osiris <rihanna.purandah25@compscihigh.org>
2025-03-25 14:30:49 +0000

    finish it

--- index.js
+++ index.js
@@ -26,27 +26,51 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
-
+app.post("/add/country", async(req,res) => {
+  const  country = await new Country({
+    name: req.body.name,
+    flagURL: req.body.flagURL,
+    population: req.body.population,
+    officialLanguage: req.body.language,
+    hasNuclearWeapons: req.body.hasNuclearWeapons
+  })
+})
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
-
+app.get("/", async(req,res) => {
+  const country = await Country.find({})
+    res.render("country.ejs", {country})
+})
 
 // Go to countries.ejs and follow the tasks there (2 points)
 
 
 // Create a dynamic PATCH route handler for "/update/{name}" that modifies the population of the country specified in the path (3 points)
 // Test this route on post man
-
+app.patch("/country/update/earth", async(req,res) =>{
+  const response = await Country.findOneAndUpdate(
+    {
+    name: req.params.name
+    },
+    {
+    population: req.params.population  
+    })
+    res.json(response)
+})
 
 
 // Create a DELETE route handler for "/delete/country" that deletes a country of your choice (3 points)
 // Test this route on post man
-
+app.delete("/country/delete/earth", async(req,res) =>{
+  const response = await Country/findOneAndDelete({
+    name: req.params.name
+  })
+})
 
 async function startServer() {
   
     // add your SRV string with a database called countries
-  await mongoose.connect("...");
+  await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.gc1au.mongodb.net/countries?retryWrites=true&w=majority&appName=Cluster0");
 
   app.listen(3000, () => {
     console.log("Server is running");
